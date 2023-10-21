b6d70ab7fde5decf8c745ac67f67da28a3abc097
Pato-Bett <patobett588@gmail.com>
2023-10-21 12:59:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers,name){
+    const result = drivers.filter((driverName)=>driverName.toUpperCase()===name.toUpperCase())
+    return result
+}
+
+function fuzzyMatch(drivers,providedName){
+    const result = drivers.filter((driverName)=>{
+        const driver = driverName.toLowerCase()
+        const provided = providedName.toLowerCase()
+        //startsWith() method is used to
+        // check if a string starts with a specified substring/letters
+        if (driver.startsWith(provided)) {
+            return true
+        }
+    })
+    return result
+}
+
+function matchName(drivers,providedName){
+    return drivers.filter((driverName)=>providedName === driverName.name)
+}
